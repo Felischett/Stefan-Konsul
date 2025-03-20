93714418304347dbb833835036aab748b36c2e82
Felischett <165804735+Felischett@users.noreply.github.com>
2025-03-20 14:46:24 +0100

    first commit

---
+++ Hlavac-Konsul/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hlavac_Konsul
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
